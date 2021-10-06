67edd61b1500104dea00a60ef12adaa32e13adc0
labibCodemen <labibhassan016@gmail.com>
2021-10-06 12:36:11 +0600

    first commit

---
+++ project/main.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    let circle = document.getElementById('circle');
+    let main = document.getElementById('main');
+    main.classList.toggle('bg-green-500');
+    circle.classList.toggle('left-14');
+
+}
